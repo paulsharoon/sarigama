690fed2ee0df046fc686a633eb2be92039a9f542
balavikraman.p <balavikraman.p@abyantra.com>
2018-11-26 22:14:03 +0530

    ui dependencies commit

---
+++ WebContent/chatUI/resources/static/app/app.js
@@ -0,0 +1,20 @@
+var app =angular.module('sariGaMa,'['ui-router','ngsanitize']);
+
+
+app.constant('urlRoutePrefix',contextPath);
+app.config(['$stateProvider', '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider) {
+
+$urlRouterProvider.otherwise('/signin');
+
+$stateProvider
+	.state('demoPage', {
+		cache:false,
+		url: '/demopage',
+		templateUrl:'pages/demo/demo.html',
+		controller:'demoCtrl'
+	})
+
+
+}]);
+
---
+++ WebContent/chatUI/resources/static/app/componentOne/controller/demo.js
@@ -0,0 +1,3 @@
+app.controller('demoCtrl',function($scope){
+$scope.demo="sariGaMa"
+});
